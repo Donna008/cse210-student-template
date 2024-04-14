c02887264537fb188ed9139ffcb857a2818e1970
Donna008 <ntang800@gmail.com>
2024-04-14 14:45:59 -0600

    added prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name: ");
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name: ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        // Console.Write("What is your first name: ");
+        // string firstName = Console.ReadLine();
+        // Console.Write("What is your last name: ");
+        // string lastName = Console.ReadLine();
+        // Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        Console.Write("Enter your last name and first name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your first name: ");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {name}, {fname}.");
+
     }
 }
